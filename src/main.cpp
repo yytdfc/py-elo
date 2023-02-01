3d6f2df1b317fa18a3ef5df495d2623b4fd84f64
yytdfc <fuchen@foxmail.com>
2023-02-01 11:30:52 +0800

    bayeselo

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,41 @@
+#include "BayesElo/CResultSet.h"
+#include "BayesElo/CEloRatingCUI.h"
+#include <sstream>
 #include <pybind11/pybind11.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
-
 namespace py = pybind11;
 
-PYBIND11_MODULE(python_example, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-
-        .. currentmodule:: python_example
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
-
+PYBIND11_MODULE(bayeselo, m) {
+  py::class_<CResultSet>(m, "ResultSet")
+      .def(py::init<>(), "Result Set")
+      .def("append", &CResultSet::Append, "append result", py::arg("white"),
+           py::arg("black"), py::arg("result"));
+  py::class_<CEloRatingCUI>(m, "EloRating")
+      .def(py::init<const CResultSet&, const std::vector<std::string>&>(),
+           "elo rating", py::arg("result_set"), py::arg("name_list"))
+      .def("mm", &CEloRatingCUI::MM, "", py::arg("theta_w") = 0,
+           py::arg("theta_d") = 0)
+      .def("exact_dist", &CEloRatingCUI::ExactDist, "", py::arg("player") = -1)
+      .def("offset", &CEloRatingCUI::Offset, "", py::arg("offset"))
+      .def("__repr__",
+           [](CEloRatingCUI& a) {
+             std::ostringstream oss;
+             a.Ratings(oss);
+             return oss.str();
+           })
+      .def("__str__", [](CEloRatingCUI& a) {
+        std::ostringstream oss;
+        a.Ratings(oss);
+        return oss.str();
+      });
 #ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-    m.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 #endif
 }
+
+
